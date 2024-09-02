2e5a81c0316dbb9a39efd944fc82f555dd7f4407
LHaitam <lourhmihaitam@outlook.fr>
2024-09-02 16:50:40 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ export default function Main() {
     return (
         <>
             <Head>
-                <title>Itamae Sushi</title>
+                <title>Eatsu</title>
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <meta name="author" content="Michał Biernacki"></meta>
                 <meta name="keywords" content="sushi, website, proposition"></meta>
-                <meta name="description" content="Sushi website proposition made by Michał Biernacki"></meta>
+                <meta name="description" content="Sushi website proposition made by Aquila TS"></meta>
             </Head>
             <Home></Home>
         </>
